26a90c273eb729850cd3df29445adf9fb010e775
Gabor Szabo <gszabo@sessiondigital.com>
2016-09-22 16:33:35 +0200

    f

--- features/data/install-magento-database.sh
+++ features/data/install-magento-database.sh
@@ -1,3 +1,3 @@
 mysql -uroot -e "CREATE DATABASE magentodb;"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON magnetodb.* TO 'magento'@'localhost' IDENTIFIED BY 'magento';"
-gunzip magento-demo.gz | mysql magentodb -uroot
+gunzip features/data/magento-demo.gz | mysql magentodb -uroot
