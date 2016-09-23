cff06ea0831f890e9ad93ec6699d1ef6b213c9e1
Gabor Szabo <gszabo@sessiondigital.com>
2016-09-23 13:46:39 +0200

    f

--- features/data/install-magento-database.sh
+++ features/data/install-magento-database.sh
@@ -1,3 +1,3 @@
-mysql -uroot -e "CREATE DATABASE magentodb;"
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS magentodb;"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON magnetodb.* TO 'magento'@'localhost' IDENTIFIED BY 'magento';FLUSH PRIVILEGES;"
 gunzip < features/data/magento-demo.sql.gz | mysql magentodb -uroot
